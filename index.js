f4965dce7dfc878d8e570412fac5b8bda2745960
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-04-22 17:20:17 -0700

    refactor to express

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
-import http from 'node:http';
-import fs from 'node:fs';
-import { pipeline } from 'node:stream';
 import path from 'node:path';
+import express from 'express';
 
-const validURLs = new Set(['/', '/about', '/contact-me']);
+const fileRouteOptions = {
+    root: path.normalize(import.meta.dirname),
+}
 
-const server = http.createServer((request, response) => {
-    request.on('error', err => {
-        console.error(err);
-        response.statusCode = 400;;
-        response.end();
-    });
-    response.on('error', err => {
-        console.error(err);
-    });
-    let fileName;
-    if (request.method === 'GET' && validURLs.has(request.url)) {
-        response.statusCode = 200;
-        if (request.url === '/') fileName = "index.html";
-        else if (request.url === '/about') fileName = "about.html";
-        else fileName = "contact-me.html";
-    } else {
-        response.statusCode = 404;
-        fileName = '404.html';
-    }
-    response.setHeader('Content-Type', 'text/html');
-    const filePath = path.join(process.cwd(), '/', fileName);
-    const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
-    pipeline(readStream, response, err => {
-        if (err) console.error(err);
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.sendFile('index.html', fileRouteOptions, (err) => {
+        if (err) next(err);
+        else console.log("Served index");
     });
 });
 
-server.on('error', err => {
+app.get('/about', (req, res, next) => {
+    res.sendFile('about.html', fileRouteOptions, (err) => {
+        if (err) next(err);
+        else console.log("Served about");
+    });
+});
+
+app.get('/contact-me', (req, res, next) => {
+    res.sendFile('contact-me.html', fileRouteOptions, (err) => {
+        if (err) next(err);
+        else console.log("Served contact-me");
+    });
+});
+
+app.use((req, res, next) => {
+    res.status(404);
+    res.sendFile('404.html', fileRouteOptions, (err) => {
+        if (err) next(err);
+        else console.log("Served 404");
+    });
+});
+
+app.on('error', err => {
     console.error("The server itself errored.");
     console.error(err);
 });
 
-server.listen(8080);
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.end();
+});
+
+app.listen(8080, err => {
+    if (err) throw err;
+});
